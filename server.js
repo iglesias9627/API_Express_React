663cbf804aed012f84d7cdc447eff157a9ff6411
Jorge Alberto Iglesias <33730243+iglesias9627@users.noreply.github.com>
2024-02-27 00:06:41 +0100

    tutorial models woth mongoose

--- server.js
+++ server.js
@@ -15,6 +15,20 @@ app.use(express.json());
 // parse requests of content- application/x-www
 app.use(express.urlencoded({extended: true}));
 
+const db = require('./models');
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to the database!');
+  })
+  .catch(err => {
+    console.log(`Cannot connect to the database! Error: ${err}`);
+    process.exit();
+  })
+
 // simple route
 app.get('/', (req,res) => {
     res.json({message:'Welcome to my application.'})
